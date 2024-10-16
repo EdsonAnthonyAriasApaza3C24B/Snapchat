b884bd1d092f23651cc945a867e72eb4bea8655e
Edson Arias <edson.arias@tecsup.edu.pe>
2024-10-16 12:43:19 -0500

    Initial Commit

---
+++ AriasSnapchat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AriasSnapchat
+//
+//  Created by EdsonArias on 16/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
